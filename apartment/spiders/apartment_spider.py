9dbb129f7ac3a4a5f025190e947eabdbf63b0ded
moreenh <moreen.14@cse.mrt.ac.lk>
2018-06-19 21:19:55 +0530

    first commit

---
+++ apartment/spiders/apartment_spider.py
@@ -0,0 +1,27 @@
+import scrapy
+
+
+class ApartmentSpider(scrapy.Spider):
+
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'https://dubai.dubizzle.com/property-for-rent/residential/apartmentflat/2015/8/5/huge-4-br-on-a-high-floor-now-available-2/?related_ads=1&back=ZHViYWkuZHViaXp6bGUuY29tL3Byb3BlcnR5LWZvci1yZW50L3Jlc2lkZW50aWFsL2FwYXJ0bWVudGZsYXQv',
+
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        for quote in response.css('div.results-list'):
+            yield {
+                'title': quote.xpath(
+                    'div/div/div/div[2]/a/div[2]/div[2]/div/div/div/h2/text()').extract_first(),
+
+
+            }
+
+        next_page = response.css('li.next a::attr("href")').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
