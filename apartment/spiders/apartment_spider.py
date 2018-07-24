f54153a3e8c8c77bf7ca8ddb30577d3ea0c76203
moreenh <moreen.14@cse.mrt.ac.lk>
2018-07-24 16:16:27 +0530

    modify crawling

--- apartment/spiders/apartment_spider.py
+++ apartment/spiders/apartment_spider.py
@@ -16,15 +16,28 @@ class ApartmentSpider(scrapy.Spider):
     def parse(self, response):
         for quote in response.css('div.results-list'):
             yield {
+                # 'title': quote.xpath(
+                #     'div/div/div/div[2]/a/div[2]/div[2]/div/div/div/h2/text()').extract(),
+                # 'place': quote.xpath(
+                #     'div/div/div/div[2]/a/div[2]/div[2]/div/div/div/div/div/span/text()').extract(),
+                # 'updatetime': quote.xpath(
+                #     'div/div/div/div[2]/a/div[2]/div[2]/div/div/div/div[2]/text()').extract(),
+                # 'place': quote.xpath(
+                #     '//*[contains(concat( " ", @class, " " ), concat( " ", "place", " " ))]/h2/text()').extract(),
                 'title': quote.xpath(
-                    'div/div/div/div[2]/a/div[2]/div[2]/div/div/div/h2/text()').extract(),
+                    '//*[contains(concat( " ", @class, " " ), concat( " ", "en", " " ))]/text()').extract_first(),
                 'place': quote.xpath(
-                    'div/div/div/div[2]/a/div[2]/div[2]/div/div/div/div/div/span/text()').extract(),
+                    '//*[contains(concat( " ", @class, " " ), concat( " ", "place", " " ))]/span/text()').extract_first(),
                 'updatetime': quote.xpath(
-                    'div/div/div/div[2]/a/div[2]/div[2]/div/div/div/div[2]/text()').extract(),
+                    '//*[contains(concat( " ", @class, " " ), concat( " ", "timeago", " " ))]/text()').extract_first(),
+                'price': quote.xpath(
+                    '//*[contains(concat( " ", @class, " " ), concat( " ", "price", " " ))]/text()').extract_first(),
+                # 'logo': quote.xpath('//img/src').extract(),
+                'photocount': quote.xpath(
+                    '//*[contains(concat( " ", @class, " " ), concat( " ", "count_value", " " ))]/text()').extract_first()
 
             }
 
-        next_page = response.css('li.next a::attr("href")').extract_first()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+            next_page = response.css('li.next a::attr("href")').extract_first()
+            if next_page is not None:
+                yield response.follow(next_page, self.parse)
